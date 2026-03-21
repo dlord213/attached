1d5b94c50babec861770aaf684190256248041cd
dlord213 <lollingworld41@gmail.com>
2026-03-21 18:28:49 +0800

    feat: Generate new app icons for Android, Web, and Windows, and update Android application ID and manifest.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.attached"
+    namespace = "com.mirimomekiku.attached"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.attached"
+        applicationId = "com.mirimomekiku.attached"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/mirimomekiku/attached/MainActivity.kt
+++ android/app/src/main/kotlin/com/mirimomekiku/attached/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.attached
+package com.mirimomekiku.attached
 
 import io.flutter.embedding.android.FlutterActivity
 
